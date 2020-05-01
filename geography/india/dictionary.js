9a12d0dc3cb9dcd2880c81a0813c93f90afc48df
Harry Hackett <elderyrrah@gmail.com>
2020-05-01 23:59:49 +1200

    Fixed spelling error

--- geography/india/dictionary.js
+++ geography/india/dictionary.js
@@ -4,7 +4,7 @@ var script = [
 ["andaman_and_nicobar","andhra_pradesh","karnataka","kerala","tamil_nadu","telangana","puducherry","lakshadweep"],
 ["goa","maharashtra","gujarat","dadra_and_nagar_haveli","diu","daman"],
 ["rajasthan","chandigarh","delhi","haryana","himachal_pradesh","jammu_and_kashmir","ladakh","punjab"],
-["uttar_pradesh","uttarakhand","madhya_pradesh","chhattisharh"],
+["uttar_pradesh","uttarakhand","madhya_pradesh","chhattisgarh"],
 ["bihar","jharkhand","odisha","west_bengal"],
 ["assam","manipur","meghalaya","mizoram","nagaland","tripura","sikkim"]
 ];
@@ -14,7 +14,7 @@ var latin = [
 ["andaman and nicobar","andhra pradesh","karnataka","kerala","tamil nadu","telangana","puducherry","lakshadweep"],
 ["goa","maharashtra","gujarat","dadra and nagar haveli","diu","daman"],
 ["rajasthan","chandigarh","delhi","haryana","himachal pradesh","jammu and kashmir","ladakh","punjab"],
-["uttar pradesh","uttarakhand","madhya pradesh","chhattisharh"],
+["uttar pradesh","uttarakhand","madhya pradesh","chhattisgarh"],
 ["bihar","jharkhand","odisha","west bengal"],
 ["assam","manipur","meghalaya","mizoram","nagaland","tripura","sikkim"]
 ];
