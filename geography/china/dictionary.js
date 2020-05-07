3b255bac5e3b0e44f147cdbefc80242a3b1eb0a4
Harry Hackett <elderyrrah@gmail.com>
2020-05-08 02:31:29 +1200

    Added China to geography

---
+++ geography/china/dictionary.js
@@ -0,0 +1,18 @@
+var script_type = Array();
+
+var script = [
+["yunnan", "guangxi", "guangdong", "sichuan", "hong_kong", "macau", "taiwan"],
+["tibet", "xinjiang", "qinhai", "gansu", "ningxia"],
+["test"],
+["test"],
+["test"]
+];
+
+
+var latin = [
+["yunnan", "guangxi", "guangdong", "sichuan", "hong kong", "macau", "taiwan"],
+["tibet", "xinjiang", "qinhai", "gansu", "ningxia"],
+["test"],
+["test"],
+["test"]
+];
