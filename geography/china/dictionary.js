494d29577154312afb02952d7bc73339f85ee590
Harry Hackett <elderyrrah@gmail.com>
2020-05-08 23:32:58 +1200

    Added all provinces to map view

--- geography/china/dictionary.js
+++ geography/china/dictionary.js
@@ -3,7 +3,7 @@ var script_type = Array();
 var script = [
 ["beijing", "tianjin", "hebei", "shanxi"],
 ["liaoning", "jilin", "heilongjiang", "inner_mongolia"],
-["shanghai", "jiangsu", "zhejiang", "anhui", "fujian", "jiangxi", "shandong", "taiwan"]
+["shanghai", "jiangsu", "zhejiang", "anhui", "fujian", "jiangxi", "shandong", "taiwan"],
 ["henan", "hubei", "hunan", "guangdong", "guangxi", "hainan", "hong_kong", "macau"],
 ["sichuan", "guizhou", "yunnan", "chongqing", "tibet"],
 ["shaanxi", "gansu", "qinghai", "ningxia", "xinjiang"]
@@ -13,7 +13,7 @@ var script = [
 var latin = [
 ["beijing", "tianjin", "hebei", "shanxi"],
 ["liaoning", "jilin", "heilongjiang", "inner mongolia"],
-["shanghai", "jiangsu", "zhejiang", "anhui", "fujian", "jiangxi", "shandong", "taiwan"]
+["shanghai", "jiangsu", "zhejiang", "anhui", "fujian", "jiangxi", "shandong", "taiwan"],
 ["henan", "hubei", "hunan", "guangdong", "guangxi", "hainan", "hong kong", "macau"],
 ["sichuan", "guizhou", "yunnan", "chongqing", "tibet"],
 ["shaanxi", "gansu", "qinghai", "ningxia", "xinjiang"]
