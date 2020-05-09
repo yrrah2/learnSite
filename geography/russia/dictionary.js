356a0c24d401368241bf09fbace77ac4e24904fd
Harry Hackett <elderyrrah@gmail.com>
2020-05-09 19:48:31 +1200

    Added Russia

---
+++ geography/russia/dictionary.js
@@ -0,0 +1,24 @@
+var script_type = Array();
+
+var script = [
+["arkhangelsk", "murmansk", "vologda", "kaliningrad", "karelia", "komi", "leningrad", "nenets", "novgorod", "pskov", "saint_petersburg"],
+["moscow"],
+["perm"],
+["volgograd"],
+["dagestan"],
+["kurgan"],
+["omsk"],
+["sakha"]
+];
+
+
+var latin = [
+["arkhangelsk", "murmansk", "vologda", "kaliningrad", "karelia", "komi", "leningrad", "nenets", "novgorod", "pskov", "saint petersburg"],
+["moscow"],
+["perm"],
+["volgograd"],
+["dagestan"],
+["kurgan"],
+["omsk"],
+["sakha"]
+];
