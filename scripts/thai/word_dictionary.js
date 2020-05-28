14d49115e588b463e467f98f0e66400f0df49fc1
Harry Hackett <elderyrrah@gmail.com>
2020-05-28 23:04:18 +1200

    Testing word pages

---
+++ scripts/thai/word_dictionary.js
@@ -0,0 +1,5 @@
+var script_type = Array();
+
+var script = [["ได้ (can)","ทำ (do)","แพง (expensive)","มี (have)","ไม่ (no)","บาท (thai currency)","คิด (think)","วันนี้ (today)","อะไร (what)","มา (come)","อย่า (don't)","ดี (good)","ซ้าย (left)","ขวา (right)","นั่น (that)","หิวน้ำ (thirsty)","พรุ่งนี้ (tomorrow)","ทำไม (why)"]]
+
+var latin = [["dai","taam","paaeng","mii","mai","baat","khid","wannii","arai","maa","yaa","dii","saai","khwaa","nan","hiwnaam","phrungnii","taammai"]]
