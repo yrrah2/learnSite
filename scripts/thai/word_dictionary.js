655eaaa4525265f459d57da0fa99cc0655fc762d
Harry Hackett <yrrah2@users.noreply.github.com>
2020-06-03 22:44:37 +1200

    Added 11 Thai words
    This is so that many letters arent ignored

--- scripts/thai/word_dictionary.js
+++ scripts/thai/word_dictionary.js
@@ -1,5 +1,5 @@
 var script_type = Array();
 
-var script = [["ได้ (can)","ทำ (do)","แพง (expensive)","มี (have)","ไม่ (no)","บาท (thai currency)","คิด (think)","วันนี้ (today)","อะไร (what)","มา (come)","อย่า (don't)","ดี (good)","ซ้าย (left)","ขวา (right)","นั่น (that)","หิวน้ำ (thirsty)","พรุ่งนี้ (tomorrow)","ทำไม (why)"]]
+var script = [["ได้ (can)","ทำ (do)","แพง (expensive)","มี (have)","ไม่ (no)","บาท (thai currency)","คิด (think)","วันนี้ (today)","อะไร (what)","มา (come)","อย่า (don't)","ดี (good)","ซ้าย (left)","ขวา (right)","นั่น (that)","หิวน้ำ (thirsty)","พรุ่งนี้ (tomorrow)","ทำไม (why)","คุณ (you)","ธุลี (dust)","ภาษา (language)","ชฎา (headress)","ปฏัก (javelin)","จิตร (design)","โสฬส (old coin)","ฑีฆายุ (long live)","ผู้เฒ่า (elder)","ผึ้ง (bee)","เฌอ (tree)"]]
 
-var latin = [["dai","taam","paaeng","mii","mai","baat","khid","wannii","arai","maa","yaa","dii","saai","khwaa","nan","hiwnaam","phrungnii","taammai"]]
+var latin = [["dai","taam","paaeng","mii","mai","baat","khid","wannii","arai","maa","yaa","dii","saai","khwaa","nan","hiwnaam","phrungnii","taammai","khun","thulii","phaasaa","chadaa","patak","citr","sols","thiikhaayu","phutaao","pheeung","choe"]]
